b8f32f7e96b9f0df5d61a0edc4ab308c26e225e6
Thanmayee167 <thanmayee167@gmail.com>
2025-03-02 22:11:56 +0530

    Demonstrated autowire="byType". In @Autowired if we have more than one similar type of bean based on primary property it takes bean as the preferred bean

--- src/main/java/com/thanmayee/springcontainerdemo/Laptop.java
+++ src/main/java/com/thanmayee/springcontainerdemo/Laptop.java
@@ -1,7 +1,12 @@
 package com.thanmayee.springcontainerdemo;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class Laptop implements Computer {
-	public void compile() {
-		System.out.println("Code Compiled in a Laptop");
-	}
+  public void compile() {
+    System.out.println("Code Compiled in a Laptop");
+  }
 }
